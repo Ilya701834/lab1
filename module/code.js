b3d9a3bd2b132705f6fad713b6977de83313ce44
Ilya Gordynets <ilyagordynets@gmail.com>
2021-10-23 15:07:32 +0300

    feat: Creating the twosDifference function

--- module/code.js
+++ module/code.js
@@ -42,3 +42,31 @@ module.exports.strictEqual = function  strictEqual (primer) {
     return `${otvet}\n`
 }
 
+module.exports.twosDifference = function  twosDifference (mas) {
+    mas = mas.slice(1,mas.length-3).split(',')
+    let newmas=[]
+    let x = 0
+    let y = 0
+    let otvet=''
+    for(let i=0;i<mas.length;i++){
+        for(let j=0;j<mas.length;j++){
+            if(+mas[i]-+mas[j]===2){
+                newmas.push([+mas[i],+mas[j]])
+            }
+        }
+    }
+    for(let i=0;i<newmas.length;i=i+2){
+        for(let j=i;j<newmas.length;j=j+2){
+            if(newmas[i]+newmas[i+1]>newmas[j]+newmas[j+1]){
+                x=newmas[i]
+                y=newmas[i+1]
+                newmas[i]=newmas[j]
+                newmas[i+1]=newmas[j+1]
+                newmas[j]=x
+                newmas[j+1]=y
+            }
+        }
+    }
+    newmas.forEach((x,i) => i%2===0 ? otvet=otvet+'['+x+',' : otvet=otvet+x+'],')
+ return `[${otvet}]`
+}
