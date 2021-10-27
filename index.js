27af544a2e0b1046ba9a68cfe078950e5a9f6640
Ilya Gordynets <ilyagordynets@gmail.com>
2021-10-27 18:45:49 +0300

    ref: Handled work with non-existent files

--- index.js
+++ index.js
@@ -5,25 +5,36 @@ const program = require('commander');
 
 
 const TransformLab = require('./module/transform');
+const {fileExistsSync} = require("./module/valid");
 
 const pipeline = util.promisify(stream.pipeline);
 
 const actions = async _ => {
     const { input, output, task } = program.opts();
 
-    process.stdout.write('Enter text or an array press ENTER. Press CRTL + C to exit. \n')
 
-    const ReadableStream = typeof(input) === 'string' ? fs.createReadStream(input) : process.stdin;
-    const WriteableStream = typeof(output) === 'string' ? fs.createWriteStream(output): process.stdout;
+
+    input === undefined && process.stdout.write('Enter the text and press ENTER to t1.1/t1.2 | press CTRL + C to exit: ')
+
+    const ReadableStream =  typeof(input) === 'string' && fileExistsSync(input) ? fs.createReadStream(input) : process.stdin;
+
+    const WriteableStream = typeof(output) === 'string' && fileExistsSync(output) ? fs.createWriteStream(output): process.stdout;
+
 
     try {
-        await pipeline(
-            ReadableStream,
-            new TransformLab(task),
-            WriteableStream
-        );
+        if (fileExistsSync(input) || fileExistsSync(output) || input === undefined || output === undefined){
+            await pipeline(
+                ReadableStream,
+                new TransformLab(task),
+                WriteableStream
+            )
+        } else{
+            process.stderr.write( `file not exist!!!\n`);
+            process.exit(2);
+        }
+
     } catch (e) {
-        process.stderr.write(` ${e.message}\n`);
+        process.stderr.write( `${e.message}\n`);
         process.exit(1);
     }
 }
@@ -33,7 +44,7 @@ process.on('exit', code => console.log('CODE: '+code));
 process.on('SIGINT', _ => { process.exit(0); });
 
 program
-    .option('-t --task <task>', 'To select task 1 or task 2')
+    .requiredOption('-t --task <task>', 'To select task 1 or task 2')
     .option('-i, --input <filename>', 'An input file')
     .option('-o, --output <filename>', 'An output file')
     .action(actions)
